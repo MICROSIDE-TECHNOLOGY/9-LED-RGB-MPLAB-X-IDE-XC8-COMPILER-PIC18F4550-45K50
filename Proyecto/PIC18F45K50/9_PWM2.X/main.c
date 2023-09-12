ebeac7221dff5d54e416df860bceca0c2f6774cd
vhperaltaj <victor.peralta@microside.com>
2023-09-12 13:59:57 -0600

    Updated indentation & comments

--- Proyecto/PIC18F45K50/9_PWM2.X/main.c
+++ Proyecto/PIC18F45K50/9_PWM2.X/main.c
@@ -19,81 +19,60 @@
 */
 
 /*
-    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
-    
-    Subject to your compliance with these terms, you may use Microchip software and any 
-    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
-    license terms applicable to your use of third party software (including open source software) that 
+    (c) 2018 Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip software and any
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party
+    license terms applicable to your use of third party software (including open source software) that
     may accompany Microchip software.
-    
-    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
-    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
-    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS
     FOR A PARTICULAR PURPOSE.
-    
-    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
-    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
-    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
-    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
-    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
-    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
-    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS
     SOFTWARE.
 */
 
 #include "mcc_generated_files/mcc.h"
 
-/*
-                         Main application
- */
-void Set_PWM (void);
-void main(void)
+void Set_PWM( void );
+
+void main( void )
 {
-    // Initialize the device
-    SYSTEM_Initialize();
+    SYSTEM_Initialize(); // Configura el dispositivo
 
-    // If using interrupts in PIC18 High/Low Priority Mode you need to enable the Global High and Low Interrupts
-    // If using interrupts in PIC Mid-Range Compatibility Mode you need to enable the Global and Peripheral Interrupts
-    // Use the following macros to:
-
-    // Enable the Global Interrupts
-    //INTERRUPT_GlobalInterruptEnable();
-
-    // Disable the Global Interrupts
-    //INTERRUPT_GlobalInterruptDisable();
-
-    // Enable the Peripheral Interrupts
-    //INTERRUPT_PeripheralInterruptEnable();
-
-    // Disable the Peripheral Interrupts
-    //INTERRUPT_PeripheralInterruptDisable();
-
-    while (1)
-    {
-        LEDR_SetHigh();
+    while ( 1 ) {
+        LEDR_SetHigh();  // Enciende el LED Rojo y apaga el resto
         LEDV_SetLow();
         LEDA_SetLow();
-        Set_PWM();
+        Set_PWM();       // Atenúa el brillo del LED
         LEDR_SetLow();
-        LEDV_SetHigh();
+        LEDV_SetHigh();  // Enciende el LED Verde y apaga el resto
         LEDA_SetLow();
         Set_PWM();
         LEDR_SetLow();
         LEDV_SetLow();
-        LEDA_SetHigh();
+        LEDA_SetHigh();  // Enciende el LED Azul y apaga el resto
         Set_PWM();
     }
 }
 
-void Set_PWM (void){
-   for (int i=0; i < 255; i++){
-    EPWM1_LoadDutyValue(i);
-    __delay_ms (5);
-   }
-    for (int i= 255; i > 0; i--){
-    EPWM1_LoadDutyValue(i);
-    __delay_ms (5);
-   }
-   
-    
+void Set_PWM( void )
+{
+    for ( int i = 0; i < 255; i++ ) {
+        EPWM1_LoadDutyValue( i );
+        __delay_ms( 5 );
+    }
+    for ( int i = 255; i > 0; i-- ) {
+        EPWM1_LoadDutyValue( i );
+        __delay_ms( 5 );
+    }
 }
